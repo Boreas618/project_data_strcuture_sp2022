2d01bdb81f3d7624a12f7f2d4326435bbe3aa1d2
Boreas618 <21307130094@m.fuadn.edu.cn>
2022-12-04 21:20:43 +0800

    feat:parse the skeleton

--- bvh_parser.cpp
+++ bvh_parser.cpp
@@ -2,12 +2,15 @@
 #include"bvh_parser.h"
 #include <iostream>
 #include <string>
+#include <stack>
+#include <sstream>
+
 // a naive bvh parser
 
 using std::ifstream;
 
 /// @brief A helper function for removing whitespace from both sides of the string
-/// @param string line 
+/// @param line the string to be processed
 /// @return A string without whitespace at both of its sides
 string trim(string line){
     int pointer_forward = 0;
@@ -36,20 +39,84 @@ string trim(string line){
     return line.substr(pointer_forward, pointer_backward+1);
 }
 
-void buildSkelenton(joint& root, char* path){
+/// @brief A function fot parsing the offset information
+/// @param line the input string
+/// @return a vector(length:3) storing the information
+vector<double> parseOffset(string line){
+    int startIndex = line.find_first_of(' ');
+    vector<double> offsetFields(3);
+    string info = line.substr(startIndex, line.size());
+    std::stringstream ss(info);
+    ss >> offsetFields[0] >> offsetFields[1] >> offsetFields[2];
+    return offsetFields;
+}
+
+/// @brief A function for parsing the channel information
+/// @param line the input string
+/// @return A vector storing the channel fields
+vector<string> parseChannels(string line){
+    int startIndex = line.find_first_of(' ');
+    int length = 0;
+    std::stringstream ss(line.substr(startIndex, line.size()));
+    ss >> length;
+    vector<string> channelFields(length);
+    for(int i = 0; i < length; i++)
+        ss >> channelFields[i];
+    return channelFields;
+}
+
+/// @brief A function for building the skeleton of the data.
+/// @param root the root node
+/// @param path the file path
+void buildSkeleton(joint& root, char* path){
+    std::stack<joint*> s;
+    joint *p = &root;
     string line;
     ifstream file(path);
     if(file.is_open()){
         int i = 0;
         while(std::getline(file, line)){
 
+            line = trim(line);
+
             //IMPORTANT: EVERY line ends with a '\r'
-            if(line=="MOTION\r")
+            if(line == "MOTION")
                 break;
 
-            if(line=="HIERARCHY\r")
+            if(line == "HIERARCHY")
                 continue;
-            std::cout<<trim(line)<<std::endl;
+            
+            if(line == "{"){
+                s.push(p);
+            } else if (line == "}"){
+                s.pop();
+            } else {
+                if(line.substr(0,line.find_first_of(' ')) == "ROOT"){
+                    p->joint_type = "ROOT";
+                }
+                if(line.substr(0,line.find_first_of(' ')) == "JOINT"){
+                    joint* childJoint = new joint();
+                    p->children.push_back(childJoint);
+                    p = childJoint;
+                    childJoint->joint_type = "JOINT";
+                }
+                if(line.substr(0,line.find_first_of(' ')) == "END"){
+                    joint* childJoint = new joint();
+                    s.top()->children.push_back(childJoint);
+                    p = childJoint;
+                    childJoint->joint_type = "END";
+                }
+                if(line.substr(0,line.find_first_of(' ')) == "OFFSET"){
+                    vector<double> offset = parseOffset(line);
+                    p->offset_x = offset[0];
+                    p->offset_y = offset[1];
+                    p->offset_z = offset[2];
+                }
+                if(line.substr(0,line.find_first_of(' ')) == "CHANNELS"){
+                    p->channels = parseChannels(line);
+                }
+            }
+            //std::cout<<trim(line)<<std::endl;
 
         }
         file.close();
@@ -60,8 +127,8 @@ int main(int argc, char** argv) {
     joint root;
     META meta_data;
     ifstream file(argv[1]);
-    buildSkelenton(root, "sample.bvh");
-    //jsonify(root, meta_data);
+    buildSkeleton(root, argv[1]);
+    jsonify(root, meta_data);
     file.close();
     return 0;
 }
