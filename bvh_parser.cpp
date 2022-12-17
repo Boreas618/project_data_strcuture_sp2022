92544dfb4114b88083ea9256e16af21fcf8cd06f
Boreas618 <21307130094@m.fudan.edu.cn>
2022-12-17 15:35:20 +0800

    optimize the parse logic

--- bvh_parser.cpp
+++ bvh_parser.cpp
@@ -10,172 +10,136 @@
 using namespace std;
 using std::ifstream;
 
-/// @brief A helper function for removing whitespace from both sides of the string
-/// @param line the string to be processed
-/// @return A string without whitespace at both of its sides
-string trim(string line){
-    int pointer_forward = 0;
-    int pointer_backward = line.size() - 1;
-
-    while(line[pointer_forward] == ' ' || 
-    line[pointer_forward] == '\n' || 
-    line[pointer_forward] == '\r'|| 
-    line[pointer_forward] == '\b'||
-    line[pointer_forward] == '\t'||
-    line[pointer_forward] == '\f'||
-    line[pointer_forward] == '\v'){
-        pointer_forward++;
-    }
-
-    while(line[pointer_backward] == ' ' || 
-    line[pointer_backward] == '\n' || 
-    line[pointer_backward] == '\r'|| 
-    line[pointer_backward] == '\b'||
-    line[pointer_backward] == '\t'||
-    line[pointer_backward] == '\f'||
-    line[pointer_backward] == '\v'){
-        pointer_backward--;
-    }
-    
-    return line.substr(pointer_forward, pointer_backward-pointer_forward+1);
-}
-
-/// @brief A function fot parsing the offset information
-/// @param line the input string
-/// @return a vector(length:3) storing the information
-vector<double> ParseOffset(const string& line){
-    int startIndex = line.find_first_of(' ');
-    vector<double> offsetFields(3);
-    string info = line.substr(startIndex, line.size()-startIndex);
-    stringstream ss(info);
-    ss >> offsetFields[0] >> offsetFields[1] >> offsetFields[2];
-    return offsetFields;
-}
-
-/// @brief A function for parsing the channel information
-/// @param line the input string
-/// @return A vector storing the channel fields
-vector<string> parseChannels(string line){
-    int startIndex = line.find_first_of(' ');
-    int length = 0;
-    stringstream ss(line.substr(startIndex, line.size()-startIndex));
-    ss >> length;
-    vector<string> channelFields(length);
-    for(int i = 0; i < length; i++)
-        ss >> channelFields[i];
-    return channelFields;
-}
-
-/// @brief A function for building the skeleton of the data.
-/// @param root the root node
-/// @param path the file path
-void buildSkeleton(joint& root, ifstream& file){
-    stack<joint*> s;
+// Build the tree skeleton of the bvh file
+// Read the leading label of each line(e.g. "ROOT", "MOTTION", ...) and parse the corresponding information that follows.
+// Use a stack to help build the tree. Whenever a "{" is met, push the current joint into
+// the stack. Whenever a "}" is met, pop the top joint from the stack. 
+// It keeps reading the file until a "MOTION" is met.
+void BuildSkeleton(joint &root, ifstream &file)
+{
+    stack<joint *> s;
     joint *p = &root;
     string line;
     string parent_name;
-    if(file.is_open()){
-        while(getline(file, line)){
-
-            line = trim(line);
-
-            if(line == "MOTION")
-                break;
-
-            if(line == "HIERARCHY")
-                continue;
-            
-            if(line == "{"){
-                s.push(p);
-            } else if (line == "}"){
-                s.pop();
-                if(!s.empty())
-                    p = s.top();
-            } else {
-                if(line.substr(0,line.find_first_of(' ')) == "ROOT"){
-                    p->joint_type = "ROOT";
-                    p->name = line.substr(line.find_first_of(' ')+1, line.size()-line.find_first_of(' ')-1);
-                    parent_name = p->name;
-                }
-                if(line.substr(0,line.find_first_of(' ')) == "JOINT"){
-                    joint* childJoint = new joint();
-                    p->children.push_back(childJoint);
-                    p = childJoint;
-                    childJoint->joint_type = "JOINT";
-                    childJoint->name = line.substr(line.find_first_of(' ')+1, line.size()-line.find_first_of(' ')-1);
-                    parent_name = childJoint->name;
-                }
-                if(line.substr(0,line.find_first_of(' ')) == "End"){
-                    joint* childJoint = new joint();
-                    p->children.push_back(childJoint);
-                    p = childJoint;
-                    p->joint_type = "END";
-                    p->name = parent_name + "_End";
-                }
-                if(line.substr(0,line.find_first_of(' ')) == "OFFSET"){
-                    vector<double> offset = ParseOffset(line);
-                    p->offset_x = offset[0];
-                    p->offset_y = offset[1];
-                    p->offset_z = offset[2];
-                }
-                if(line.substr(0,line.find_first_of(' ')) == "CHANNELS"){
-                    p->channels = parseChannels(line);
-                }
-            }
-
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string leading;
+        ss >> leading;
+        if (leading == "MOTION")
+        {
+            break;
+        }
+        else if (leading == "{")
+        {
+            s.push(p);
+        }
+        else if (leading == "}")
+        {
+            s.pop();
+            if (!s.empty())
+                p = s.top();
+        }
+        else if (leading == "ROOT")
+        {
+            p->joint_type = "ROOT";
+            ss >> p->name;
+            parent_name = p->name;
+        }
+        else if (leading == "JOINT")
+        {
+            joint *child_joint = new joint();
+            p->children.push_back(child_joint);
+            p = child_joint;
+            child_joint->joint_type = "JOINT";
+            ss >> child_joint->name;
+            parent_name = child_joint->name;
+        }
+        else if (leading == "End")
+        {
+            joint *child_joint = new joint();
+            p->children.push_back(child_joint);
+            p = child_joint;
+            p->joint_type = "END";
+            p->name = parent_name + "_End";
+        }
+        else if (leading == "OFFSET")
+        {
+            ss >> p->offset_x;
+            ss >> p->offset_y;
+            ss >> p->offset_z;
+        }
+        else if (leading == "CHANNELS")
+        {
+            int length;
+            ss >> length;
+            vector<string> channel_fields(length);
+            for (int i = 0; i < length; i++)
+                ss >> channel_fields[i];
+            p->channels = channel_fields;
         }
     }
 }
 
-/// @brief A function for generating the meta data
-/// @param meta_data 
-/// @param file 
-void generateMeta(META& meta_data, ifstream& file){
+// Generate meta data from "Frames:	433 Frame Time:	0.00833333"
+void GenerateMeta(META &meta_data, ifstream &file)
+{
     string line_frames;
     string line_frame_time;
     getline(file, line_frames);
     getline(file, line_frame_time);
 
-    stringstream ss1(line_frames.substr(line_frames.find_first_of(':')+2, line_frames.size()-line_frames.find_first_of(':')-2));
+    stringstream ss1(line_frames.substr(line_frames.find_first_of(':') + 2, line_frames.size() - line_frames.find_first_of(':') - 2));
     ss1 >> meta_data.frame;
 
-    stringstream ss2(line_frame_time.substr(line_frame_time.find_first_of(':')+2, line_frame_time.size()-line_frame_time.find_first_of(':')-2));
+    stringstream ss2(line_frame_time.substr(line_frame_time.find_first_of(':') + 2, line_frame_time.size() - line_frame_time.find_first_of(':') - 2));
     ss2 >> meta_data.frame_time;
 }
 
-void loadMotions(ifstream& file, joint& root, int frame){
+// Load the motion data
+// Tarverse the skeleton that has constrcuted earlier. Load the single-frame data in the form of a vector.
+void LoadMotions(ifstream &file, joint &root, int frame)
+{
     string line;
     joint *p;
-    stack<joint*> s;
-    while(getline(file, line)){
+    stack<joint *> s;
+    while (getline(file, line))
+    {
         p = &root;
         s.push(p);
 
         stringstream ss(line);
-        while(!s.empty()){
+        while (!s.empty())
+        {
             p = s.top();
             s.pop();
 
-            for(int i = p->children.size()-1; i >=0; i--){
+            for (int i = p->children.size() - 1; i >= 0; i--)
                 s.push(p->children[i]);
-            }
 
-            vector<double> singleFrameChannelData(p->channels.size());
-            for(int i = 0; i < p->channels.size(); i++)
-                ss >> singleFrameChannelData[i];
+            vector<double> single_frame_channel_data(p->channels.size());
 
-            p->motion.push_back(singleFrameChannelData);
+            for (int i = 0; i < p->channels.size(); i++)
+                ss >> single_frame_channel_data[i];
+
+            p->motion.push_back(single_frame_channel_data);
         }
     }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     joint root;
     META meta_data;
     ifstream file(argv[1]);
-    buildSkeleton(root, file);
-    generateMeta(meta_data,file);
-    loadMotions(file, root, meta_data.frame);
+
+    if (!file.is_open())
+        cout << "Error: failed to open" << argv[1] << endl;
+    else
+        BuildSkeleton(root, file);
+
+    GenerateMeta(meta_data, file);
+    LoadMotions(file, root, meta_data.frame);
     jsonify(root, meta_data);
     file.close();
     return 0;
